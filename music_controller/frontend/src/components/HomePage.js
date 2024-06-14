003c8e9b42a5d018afbe1876ff11dc1a46dd9a3b
Eddie Mapes <emapes@erisarecovery.com>
2024-06-14 15:03:05 -0500

    Second commit

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Redirect } from 'react-router-dom';
 
 import CreateRoomPage from './CreateRoomPage';
 import RoomJoinPage from './RoomJoinPage';
+
+const TestingComponent = () => <p>TESTING TEST</p>
 
 
 export default class HomePage extends Component {
@@ -11,6 +13,13 @@ export default class HomePage extends Component {
     }
 
     render() {
-        return <p>This is the home page</p>;
+        return (
+        <Router>
+            <Routes>
+                <Route exact path='/' element={<TestingComponent />} />
+                <Route path='/join' component={RoomJoinPage} />
+                <Route path='/create' component={CreateRoomPage} />
+            </Routes>
+        </Router>);
     }
 }
